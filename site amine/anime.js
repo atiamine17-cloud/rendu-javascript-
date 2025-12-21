b6837170af78c8113f9df9756d2bcbf3bf055f0b
atiamine17-cloud <ati.amine.17@gmail.com>
2025-12-21 17:28:34 +0100

    Add files via upload

---
+++ site amine/anime.js
@@ -0,0 +1,24 @@
+const bouton = document.getElementById("monBouton");
+const body = document.body;
+bouton.addEventListener("click", function() {
+   body.classList.toggle("mode-sombre");
+    if (body.classList.contains("mode-sombre")) {
+        bouton.textContent = "Activer le mode clair";
+    } else {
+        bouton.textContent = "Activer le Dark Mode";
+    }
+});
+
+const tabs = document.querySelectorAll('.tab-btn');
+const contents = document.querySelectorAll('.tab-content');
+
+tabs.forEach(tab => {
+    tab.addEventListener('click', () => {
+        tabs.forEach(t => t.classList.remove('active'));
+        contents.forEach(c => c.classList.remove('active'));
+        tab.classList.add('active');
+        const targetId = tab.getAttribute('data-tab');
+        document.getElementById(targetId).classList.add('active');
+    });
+});
+    
