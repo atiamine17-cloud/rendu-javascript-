38077aa1bf7443129b1483521067aad83abcb7fb
atiamine17-cloud <ati.amine.17@gmail.com>
2026-05-04 23:16:16 +0200

    Update logout.php

--- exercice_login/logout.php
+++ exercice_login/logout.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
-session_unset(); // Supprime les variables de session[cite: 1]
-session_destroy(); // Détruit la session[cite: 1]
+session_unset(); 
+session_destroy(); 
 
-header('Location: login.php'); // Retour au départ[cite: 1]
+header('Location: login.php'); 
 exit;
